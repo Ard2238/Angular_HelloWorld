0239e2f2904eff8c7d8035c1238a81135f24c862
Ard2238 <abhishekrdas28@gmail.com>
2020-12-16 01:18:55 +0530

    Added Code to demonstrate interpolation i.e one-way data binding technique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'HellWoWorld';
+  title = 'Hello World';
+
+  ngOnInit(): void{
+    this.title = "Hello From Bridgelabz";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
+// app.module.ts -- Indicating  this is a typescript file. Essentially provides strong type checking
+
+
+// Importing core modules of Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Whenever CLI is used to separate new components and services, it
+// will automatically update this file to import and add them here.
 import { AppComponent } from './app.component';
 
+// NgModule:  decorator ~~ annotations
+// Decorator: to provide additional metadata to specify 
+// the Components, the Services, the Imports, etc.. 
+// Used while processing, instantiation and runtime.
 @NgModule({
+
+  // Components are added here. Essentially the classes that has views.
   declarations: [
     AppComponent
   ],
+
+  // Imports needed for application
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+
+  // Services like http
   providers: [],
+
+  // Root component which has the main view of the application
   bootstrap: [AppComponent]
 })
 export class AppModule { }
