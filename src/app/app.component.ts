a27d27d0a706734678291784494a29a6e83dc162
Ard2238 <abhishekrdas28@gmail.com>
2020-12-16 01:47:28 +0530

    Refactored Code to demonstrate event binding of clicking on the logo image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Hello World';
   imgUrl = "";
+  url = "";
+
   ngOnInit(): void{
     this.title = "Hello From Bridgelabz";
 
     // Property-Binding: path to the property represented by the variable imgUrl
     this.imgUrl = "../assets/logo-2.png"
+
+    // For Event Binding
+    this.url = "https://www.google.com";
+  }
+
+  onClick($event){
+    console.log("Image Clicked", $event);
+    // _blank: opens in a new tab
+    window.open(this.url, "_blank");
   }
 }
