54c32bc7dc00956a00d1ededa94d154f9ed07320
Ard2238 <abhishekrdas28@gmail.com>
2020-12-16 01:33:42 +0530

    Refactored Code to demostrate property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hello World';
-
+  imgUrl = "";
   ngOnInit(): void{
     this.title = "Hello From Bridgelabz";
+
+    // Property-Binding: path to the property represented by the variable imgUrl
+    this.imgUrl = "../assets/logo-2.png"
   }
 }
